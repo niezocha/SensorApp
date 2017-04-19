abd013ba3a6ef56789232092bc8bbcbb07a9269e
Zofia <zofjanicka@gmail.com>
2017-04-19 20:10:11 +0200

    addedsensor menager for accelerometer

--- app/src/main/java/com/example/zofia/sensorapp/MainActivity.java
+++ app/src/main/java/com/example/zofia/sensorapp/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.zofia.sensorapp;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        Sensor defaultSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                Log.e("X-Axis", String.valueOf(event.values[0]));
+                Log.e("Y-Axis", String.valueOf(event.values[1]));
+                Log.e("Z-Axis", String.valueOf(event.values[2]));
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        }, defaultSensor, 1000000);
+
+
     }
 }
